14a6f746cce133e0d29d4d4a36846e7bed30a2ac
DESKTOP-PGFF3OP\Julian <julianz.c@hotmail.com>
2023-09-08 23:19:03 -0500

    update user and pat

--- build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"co.edu.uco.comun.exceptions"},{"l":"co.edu.uco.comun.handler"},{"l":"co.edu.uco.comun.validator"}];updateSearchResults();
